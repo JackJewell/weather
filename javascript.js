f399da5b894c27ac8e38ba3aa7fbf9260209cfc7
Jackson Jewell <jackson.t.jewell@gmail.com>
2019-12-05 16:38:31 -0600

    Functionality 95% done

--- javascript.js
+++ javascript.js
@@ -15,11 +15,12 @@ function buildQuery(caseVal){
                     url: queryURL,
                     method: "GET"
                 }).then(function(response){
-                    lat = response.coord.lat
-                    long = response.coord.lon
                     responseVar = response;
+                    lat = responseVar.coord.lat
+                    long = responseVar.coord.lon
                     console.log(responseVar)
                     buildPage(responseVar);
+                    buildQuery("uv");
                     return lat,long;
                 });
                 
@@ -34,8 +35,11 @@ function buildQuery(caseVal){
                     method: "GET"
                 }).then(function(response){
                     responseVar = response;
+                    lat = responseVar.coord.lat
+                    long = responseVar.coord.lon
                     console.log(responseVar)
                     buildPage(responseVar);
+                    buildQuery("uv");
                 });
             }
             
@@ -43,18 +47,16 @@ function buildQuery(caseVal){
         case "uv":
             console.log(lat);
             console.log(long);
-            //lat = responseVar.coord.lat
-            //lon = responseVar.coord.lon
             queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=6bd5f328c4eb31862977239b636ff37a"
             +"&lat="+lat+"&lon="+long;
-            //$.ajax({
-            //    url: queryURL,
-            //    method: "GET"
-            //}).then(function(response){
-            //    responseVar = response;
-           //     console.log(responseVar)
-            //    finishPage(responseVar);
-            //});
+            $.ajax({
+               url: queryURL,
+            method: "GET"
+            }).then(function(response){
+                responseVar = response;
+                console.log(responseVar)
+                finishPage(responseVar);
+            });
             break;
         case "fiveDay":
             queryURL ="http://api.openweathermap.org/data/2.5/forecast?q="
@@ -79,14 +81,9 @@ function startSearch(searchV){
     if(searchV !== undefined){
         searchItem = searchV.value;
     }else{
-
     }
-    
     buildQuery("norm");
     buildQuery("fiveDay");
-    buildQuery("uv");
-
-    
 }
 
 function buttonBuilder(buttonName){
@@ -100,27 +97,63 @@ function buttonBuilder(buttonName){
 
 function buildPage(weatherData){
     let name = weatherData.name;
-    //let weatherSymbol = weatherData.weather.children[0].icon;
-    let temperature = "Temperature: " + weatherData.main.temp + " F";
+    let weatherSymbol = weatherData.weather['0'].icon;
+    let temperature = weatherData.main.temp;
+    temperature = ((temperature * (9/5)) - 459.67);
+    temperature = temperature.toString();
+    temperature = temperature.substring(0,5);
+    temperature = "Temperature: " + temperature + " F";
     let humidity = "Humidity: " + weatherData.main.humidity + "%";
     let windSpeed = "Wind Speed: " + weatherData.wind.speed + " MPH";
     lat = weatherData.coord.lat;
     long = weatherData.coord.lon;
 
     let nameHTML = $("<h1></h1>").text(name);
+    let weatherSymbolHTML = $("<img>").attr("src","http://openweathermap.org/img/w/" + weatherSymbol + ".png");
     let temperatureHTML = $("<p></p>").text(temperature);
     let humidityHTML = $("<p></p>").text(humidity);
     let windSpeedHTML = $("<p></p>").text(windSpeed);
     $("#pageBlock").append(nameHTML);
+    $("#pageBlock").append(weatherSymbolHTML);
     $("#pageBlock").append(temperatureHTML);
     $("#pageBlock").append(humidityHTML);
     $("#pageBlock").append(windSpeedHTML);
 }
 
-function finishPage(){
+function finishPage(uvData){
+    let uvIndex = "UV Index: " + uvData.value;
+    let uvHTML = $("<p></p>").text(uvIndex);
+    $("#pageBlock").append(uvHTML);
 }
 
-function fiveDayBuilder(){
-    
-}
+function fiveDayBuilder(forecast){
+    let days = [forecast.list['4'], 
+                forecast.list['12'], 
+                forecast.list['20'], 
+                forecast.list['28'], 
+                forecast.list['36']];
+    let ticker = 0;
+    days.forEach(function(buildVar){
+        ticker++;
+        console.log(buildVar);
+        let dateHolder = buildVar.dt_txt;
+        console.log(dateHolder);
+        dateHolder = dateHolder.slice(0,10);
+        let forecastTemp = buildVar.main.temp;
+        forecastTemp = ((forecastTemp * (9/5)) - 459.67);
+        forecastTemp = forecastTemp.toString();
+        forecastTemp = forecastTemp.substring(0,5);
+        let forecastHumidity = buildVar.main.humidity
+        let forecastSymbol = buildVar.weather['0'].icon;
+        let idGetter = "#forecastCard" + ticker;
 
+        let dateHTML = $("<h3></h3>").text(dateHolder);
+        let forecastSymbolHTML = $("<img>").attr("src","http://openweathermap.org/img/w/" + forecastSymbol + ".png");
+        let forecastTempHTML = $("<p></p>").text(forecastTemp);
+        let forecastHumidityHTML = $("<p></p>").text(forecastHumidity);
+        $(idGetter).append(dateHTML);
+        $(idGetter).append(forecastSymbolHTML);
+        $(idGetter).append(forecastTempHTML);
+        $(idGetter).append(forecastHumidityHTML);
+    });
+}
